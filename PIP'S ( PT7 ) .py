8f0274ee261b2c7efc6cd59b2c35a77e680cbb24
tarang-jpg <nalinpatel7456@gmail.com>
2024-09-19 14:24:55 +0530

    Add files via upload

---
+++ PIP'S ( PT7 ) .py
@@ -0,0 +1,41 @@
+import os
+print("This Installation Takes 10-20 Min Please Wait Don't Close The App Before Installation ")
+A = "\033[1;91m"
+B = "\033[1;96m"
+C = "\033[1;97m"
+E = "\033[1;92m"
+H = "\033[1;93m"
+L = "\033[1;95m"
+M = "\033[1;94m"
+print(f'{A}={B}={C}={E}={H}={L}={M}={A}={B}={C}={E}={H}={L}={M}={A}={B}={C}={E}={H}={L}={M}='*2)
+os.system('pip install mechanize')
+os.system('pip install requests')
+os.system('pip install pyfiglet')
+os.system('pip install sys')
+os.system('pip install py_compile')
+os.system('pip install youtube_dl')
+os.system('pip install uuid')
+os.system('pip install time')
+os.system('pip install os')
+os.system('pip install random')
+os.system('pip install datetime')
+os.system('pip install string')
+print("        Wait 5-10 Min Left      ")
+os.system('pip install secrets')
+os.system('pip install webbrowser')
+os.system('pip install hashlib')
+os.system('pip install colorama')
+os.system('pip install sleep')
+os.system('pip install bs4')
+os.system('pip install json')
+os.system('pip install threading')
+os.system('pip install render')
+os.system('pip install cfonts')
+os.system('pip install InstagramAPI')
+os.system('pip install sleep')
+os.system('pip install generate_user_agent')
+os.system('pip install pysocks')
+os.system('pip install curl2pyreqs')
+os.system('cls' if os.name == 'nt' else 'clear') 
+print("≠"*60)
+print("The Installation Is Complete Now You Can Run Any Jacking File Without Any Error    Join @PythonTools7 For Best Jacking Tools ")
